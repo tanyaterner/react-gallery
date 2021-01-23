f72b09d304179ec4b4fa2cc8a83c3e8d6d8f2e88
Tanya Terner <tanyaterner010187@gmail.com>
2021-01-23 15:45:00 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-class App extends React.Component {
 
-  render() {
+function App() {
+    const [value, setValue] = useState('');
+    const [albums, setAlbums] = useState([]);
+    const [selectedAlbums, setSelectedAlbums] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/albums')
+            .then(res => res.json())
+            .then(res => (setAlbums(res)))
+    }, [])
+
+
+    function handleChange(e) {
+        const value = e.target.value
+        setValue(value)
+        fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + value)
+            .then(res => res.json())
+            .then(res => setSelectedAlbums(res))
+    }
+
+
     return (
         <div className="App">
-          <h1>Select an album:</h1>
+            <h1>Select an album:</h1>
+            <form>
+                <select value={value} onChange={handleChange}>
+                    <option>Please select</option>
 
+                    {albums.map((album) => {
+                        return <option key={album.id} value={album.id}>{album.title}</option>
+                    })}
+                </select>
+            </form>
+            <div className="img">
+                <div className="container">{selectedAlbums.map((img, index) => (
+                    <div key={index}>
+                        <img src={img.url} alt={img.title}/>
+                    </div>
+
+                ))}</div>
+            </div>
         </div>
     );
-  }
+
 
 }
 
